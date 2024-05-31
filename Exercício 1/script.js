ac53739305f9d39f3968590385e7a19b739a6a17
Fernanda-Chmiel <135976905+Fernanda-Chmiel@users.noreply.github.com>
2024-05-31 17:43:59 -0300

    Delete Exercício 1 directory

--- Exercício 1/script.js
+++
@@ -1,5 +0,0 @@
-const Paragrafo = document.getElementById("Paragrafo");
-var botao = document.getElementById("Botao");
-botao.addEventListener("click",function(){
-    Paragrafo.textContent = "Botão Clicado";
-});
